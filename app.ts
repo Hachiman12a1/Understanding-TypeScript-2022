dfd71489957d7bff5518508e36e14de3aea7e721
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-04 15:16:27 +0700

    24. Type Aliases / Custom Types

--- app.ts
+++ app.ts
@@ -1,10 +1,16 @@
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-string";
 function combine(
-  n1: number | string,
-  n2: number | string,
-  resultConversion: 'as-number' | 'as-string'
+  n1: Combinable,
+  n2: Combinable,
+  resultConversion: ConversionDescriptor
 ) {
   let result;
-  if (typeof n1 === "number" && typeof n2 === "number" && resultConversion === "as-number") {
+  if (
+    typeof n1 === "number" &&
+    typeof n2 === "number" &&
+    resultConversion === "as-number"
+  ) {
     result = +n1 + +n2;
   } else {
     result = n1.toString() + n2.toString();
@@ -12,11 +18,11 @@ function combine(
   return result;
 }
 
-const combineAges = combine(30, 26, 'as-number');
+const combineAges = combine(30, 26, "as-number");
 console.log(combineAges);
 
-const combineAges1 = combine("30", "26", 'as-number');
+const combineAges1 = combine("30", "26", "as-number");
 console.log(combineAges);
 
-const combineNames = combine("Max", "Anna", 'as-string');
+const combineNames = combine("Max", "Anna", "as-string");
 console.log(combineNames);
