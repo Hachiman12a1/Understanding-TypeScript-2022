bdb88184c6c2b6f35619a5cc9badf6c2109a35fd
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-04 16:18:21 +0700

    30. The "never" Type

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-let userInput : unknown;
-let userName : string;
+let userInput: unknown;
+let userName: string;
 
 userInput = 5;
-userInput = 'Max';
+userInput = "Max";
 
-if(typeof userInput === 'string') {
-    userName = userInput;
+if (typeof userInput === "string") {
+  userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError("An error occured", 500);
