42e07b3e835eff8a20a80f263f84cb297e41c756
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-04 15:08:12 +0700

    23. Literal Types

--- app.ts
+++ app.ts
@@ -1,15 +1,22 @@
-function combine(n1: number | string, n2: number | string) {
+function combine(
+  n1: number | string,
+  n2: number | string,
+  resultConversion: 'as-number' | 'as-string'
+) {
   let result;
-  if (typeof n1 === "number" && typeof n2 === "number") {
-    result = n1 + n2;
+  if (typeof n1 === "number" && typeof n2 === "number" && resultConversion === "as-number") {
+    result = +n1 + +n2;
   } else {
     result = n1.toString() + n2.toString();
   }
   return result;
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, 'as-number');
 console.log(combineAges);
 
-const combineNames = combine("Max", "Anna");
+const combineAges1 = combine("30", "26", 'as-number');
+console.log(combineAges);
+
+const combineNames = combine("Max", "Anna", 'as-string');
 console.log(combineNames);
