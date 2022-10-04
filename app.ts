6f1191788a873235ef7cc0c39658d9d192e39a56
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-04 14:20:17 +0700

    16. Object Types

--- app.ts
+++ app.ts
@@ -1,15 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Dat",
+  age: 30,
+};
 
-const number1 = 5;
-const number2 = 3;
-const printResult = true;
-const phraseResult = "Result is: ";
-
-add(number1, number2, printResult, phraseResult);
+console.log(person.name);
