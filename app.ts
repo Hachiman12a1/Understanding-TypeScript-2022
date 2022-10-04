2299675141903cbff504ec7d9da6530ba19386c0
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-04 14:27:36 +0700

    18. Arrays Types

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+const person = {
   name: "Dat",
   age: 30,
+  hobbies : ['Football', 'Anime']
 };
 
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
