e9da6ad2ed8841f31ab3a27d5d0670d768928b66
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-04 14:45:59 +0700

    20. Working with Enums

--- app.ts
+++ app.ts
@@ -1,12 +1,17 @@
+enum Role {ADMIN = 'ADMIN', USER = 'USER', AUTHOR = "AUTHOR"}
+
 const person = {
   name: "Dat",
   age: 30,
   hobbies : ['Football', 'Anime'], 
-  role : [2, "author"]
+  role : Role.ADMIN
 };
 
 console.log(person.name);
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
+}
+if(person.role == Role.ADMIN) {
+    console.log('Role is Admin');
 }
