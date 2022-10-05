b319d364290a22cb13e53658e35d27f31374866e
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-05 13:46:46 +0700

    53. Rest Parameters

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
-const add = (a: number, b: number) => a + b;
+// const add = (a: number, b: number) => a + b;
 
-const printOutput: (a: number | string) => void = (output) =>
-  console.log(output);
+// const printOutput: (a: number | string) => void = (output) =>
+//   console.log(output);
 
-printOutput(add(1, 2));
+// printOutput(add(1, 2));
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+const addedNumbers = add(5, 10, 20, 30);
+console.log(addedNumbers);
