21bc563c35e31e4849712e5acabbd3c8dfed0f95
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-05 15:17:48 +0700

    62. "private" and "public" Access Modifiers

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,31 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
   }
 
-  describe(this : Department) {
-    console.log('Department: ' + this.name);
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmpleyeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accounting = new Department("Accounting");
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.employees[2] = 'Anna';
 
-accounting.describe()
+accounting.describe();
+accounting.printEmpleyeeInfo()
 
-const accountingCopy = {name : 'DUMMY', describe : accounting.describe};
-accountingCopy.describe();
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+// accountingCopy.describe();
