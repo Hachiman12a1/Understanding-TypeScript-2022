fc2586847c76b8a7a11126242bea383e58a81b6f
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-06 16:45:31 +0700

    77. Interfaces as Function Types

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 interface AddFn {
-  (a: number, b: number) : number ;
+  (a: number, b: number): number;
 }
 
-let add : AddFn;
+let add: AddFn;
 
-add = (n1 : number, n2 : number) => {
+add = (n1: number, n2: number) => {
   return n1 + n2;
-}
+};
 
 interface Named {
   readonly name: string;
