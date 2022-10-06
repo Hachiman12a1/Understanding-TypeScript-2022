a19803584c7bf1a6636ea838d19cf1d02fc749c5
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-06 16:52:41 +0700

    78. Optional Parameters & Properties

--- src/app.ts
+++ src/app.ts
@@ -9,18 +9,20 @@ add = (n1: number, n2: number) => {
 };
 
 interface Named {
-  readonly name: string;
+  readonly name?: string;
 }
 interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
