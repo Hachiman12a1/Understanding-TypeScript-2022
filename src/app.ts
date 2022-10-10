0ba7ce94f4b847fbcd7eaee038b960200c7afbc7
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-10 16:38:14 +0700

    99. Generic Classes

--- src/app.ts
+++ src/app.ts
@@ -40,3 +40,35 @@ function extractAndConvert<T extends object, U extends keyof T>(
   return "Value: " + obj[key];
 }
 console.log(extractAndConvert({ name: "Max" }, "name"));
+
+class DataStorage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
+
+const objStorage = new DataStorage<object>();
+const maxObj = { name: "Max" };
+objStorage.addItem(maxObj);
+objStorage.addItem({ name: "Manu" });
+
+objStorage.removeItem(maxObj);
+console.log(objStorage.getItems());
