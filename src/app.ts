3b5ba0f17fa2e4a945e51d67e212f2e3bc48dc9f
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-10 09:56:39 +0700

    84. More on Type Guards

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,75 @@ type Admin = {
 
 type Employee = {
   name: string;
-  startDate : Date;
-}
+  startDate: Date;
+};
 
 type ElevatedEmployee = Admin & Employee;
 
-const el : ElevatedEmployee = {
-  name : 'Max',
-  privileges : ['create-server'],
-  startDate : new Date(),
-}
+const el: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
 
 type Combinabled = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinabled & Numeric;
+
+function add(a: Combinabled, b: Combinabled) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+
+  if ("privileges" in emp) {
+    console.log("privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("StartDate: " + emp.startDate);
+  }
+}
+
+printEmployeeInfo({
+  name: "Dat",
+  privileges: ["Intern React"],
+  startDate: new Date(),
+});
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading Cargo... " + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
