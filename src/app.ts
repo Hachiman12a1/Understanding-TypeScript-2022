327197389e4525ebd07a588adb4535c57cf3b4d7
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-10 17:39:46 +0700

    101. Generic Utility Types

--- src/app.ts
+++ src/app.ts
@@ -72,3 +72,24 @@ objStorage.addItem({ name: "Manu" });
 
 objStorage.removeItem(maxObj);
 console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Max", "Anna"];
