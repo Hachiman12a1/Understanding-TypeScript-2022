60def58ebce3ef4317c7c8b306946166b17d2c54
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-11 10:15:37 +0700

    107. Building More Useful Decorators

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,24 @@
-function Logger(logString : string) {
+function Logger(logString: string) {
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
   };
 }
 
-@Logger('LOGGING - PERSON')
+function withTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger('LOGGING - PERSON')
+@withTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Max";
 
