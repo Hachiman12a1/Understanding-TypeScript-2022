ad4f5f90dad0f232b726cc9bc6bb620bd4486484
Dao Van Dat <daovandat2k15@gmail.com>
2022-10-11 11:53:35 +0700

    110. Accessor & Parameter Decorators

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,63 @@
-function Logger(logString: string) {
-  console.log("LOGGER FACTORY");
-  return function (constructor: Function) {
-    console.log(logString);
-    console.log(constructor);
-  };
-}
+// function Logger(logString: string) {
+//   console.log("LOGGER FACTORY");
+//   return function (constructor: Function) {
+//     console.log(logString);
+//     console.log(constructor);
+//   };
+// }
 
-function withTemplate(template: string, hookId: string) {
-  console.log("TEMPLATE FACTORY");
-  return function (constructor: any) {
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
+// function withTemplate(template: string, hookId: string) {
+//   console.log("TEMPLATE FACTORY");
+//   return function (constructor: any) {
+//     const hookEl = document.getElementById(hookId);
+//     const p = new constructor();
 
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
-    }
-  };
-}
+//     if (hookEl) {
+//       hookEl.innerHTML = template;
+//       hookEl.querySelector("h1")!.textContent = p.name;
+//     }
+//   };
+// }
 
-@Logger("LOGGING")
-@withTemplate("<h1>My Person Object</h1>", "app")
-class Person {
-  name = "Max";
+// @Logger("LOGGING")
+// @withTemplate("<h1>My Person Object</h1>", "app")
+// class Person {
+//   name = "Max";
 
-  constructor() {
-    console.log("Creating person object...");
-  }
-}
-const pers = new Person();
+//   constructor() {
+//     console.log("Creating person object...");
+//   }
+// }
+// const pers = new Person();
 
-
-function Log(target: any, propertyName : string | Symbol) {
-  console.log('Property decorator!');
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
   console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(position);
 }
 
 class Product {
@@ -45,7 +69,7 @@ class Product {
     this.title = t;
     this._price = p;
   }
-
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
@@ -54,7 +78,11 @@ class Product {
     }
   }
 
+  @Log3
   getPriceWithTax(tax: number) {
     return this._price * (1 + tax);
   }
 }
+
+const p1 = new Product("Book 1", 10);
+const p2 = new Product("Book 2", 20);
